0026693b3d2a49ecd75c1e0cdd61b2b689c0157b
Karolina Madej <kkarolinamadej@gmail.com>
2018-08-23 12:27:23 +0200

    Add players list tests

---
+++ src/components/PlayersList/PlayersList.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import PlayersList from './PlayersList';
+import Player from '../Player/Player';
+
+it('renders without crashing', () => {
+  shallow(<PlayersList players={[]} />);
+});
+
+
+it('renders correct number of players', () => {
+  const players = [{name: 'Anna', score: 5}, {name: 'Jan', score: 0}];
+  const playerComponent = shallow(<PlayersList players={players} />);
+
+  const expectedPlayersNumber = playerComponent.find(Player).length;
+  expect(expectedPlayersNumber).toEqual(2);
+});
+
+it('should call onScoreUpdate with 10 on when first Player score is changed', () => {
+  const players = [{name: 'Anna', score: 5}, {name: 'Jan', score: 0}];
+
+  const mockedOnScoreUpdate = jest.fn();
+  const playerCopomnent = shallow(<PlayersList players={players} onScoreUpdate={mockedOnScoreUpdate} />);
+  const firstPlayer = playerCopomnent.find(Player).first();
+  const onPlayerScoreChange = firstPlayer.prop('onPlayerScoreChange');
+  onPlayerScoreChange(10);
+
+  expect(mockedOnScoreUpdate).toBeCalledWith(0, 10);
+});
