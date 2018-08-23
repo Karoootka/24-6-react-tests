b91fc3fa96a3433e61d37b08df5da37de78d61b7
Karolina Madej <kkarolinamadej@gmail.com>
2018-08-23 13:46:58 +0200

    Add next adding player tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 import PlayersList from './components/PlayersList/PlayersList';
+import AddPlayer from './components/AddPlayer/AddPlayer';
 
 
 it('renders whitout crashing', () => {
   shallow(<App />);
 });
 
-it('should update player scor', () => {
+it('should update player score', () => {
   const appComponent = shallow(<App />);
 
   const players = [{name: 'John', score: 0}];
@@ -22,4 +23,16 @@ it('should update player scor', () => {
 
   expect(playersAfterUpdate[0].score).toEqual(5);
 
-})
+});
+
+it('should update players state', () => {
+  const appComponent = shallow(<App />);
+  const onPlayerAdd = appComponent.find(AddPlayer).prop('onPlayerAdd');
+  onPlayerAdd('Adam');
+
+  const players = appComponent.state('players');
+
+  expect(players.length).toEqual(1);
+  expect(players[0].name).toEqual('Adam');
+  expect(players[0].score).toEqual(0);
+});
