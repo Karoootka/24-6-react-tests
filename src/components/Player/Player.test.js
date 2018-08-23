9226533318543b08a41d7d6e113ef593b756cdf5
Karolina Madej <kkarolinamadej@gmail.com>
2018-08-23 11:47:24 +0200

    Add player name and score tests

---
+++ src/components/Player/Player.test.js
@@ -0,0 +1,25 @@
+import Player from './Player';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+it('render without crashing', () => {
+  shallow(<Player />);
+});
+
+it('renders correct name', () => {
+  const playerNamePassed = 'Ania';
+  const playerComponent = shallow(<Player name={playerNamePassed} />);
+
+  const playerNameRendered = playerComponent.find('.Player_name').text();
+
+  expect(playerNameRendered).toEqual(playerNamePassed);
+});
+
+it('renders correct score', () => {
+  const playerScorePassed = 15;
+  const playerComponent = shallow(<Player score={playerScorePassed} />);
+
+  const playerScoreRendered = Number(playerComponent.find('.Player_score').text());
+
+  expect(playerScoreRendered).toEqual(playerScorePassed);
+});
